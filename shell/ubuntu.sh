fbd1e066d2a1cf50498e3157dbf1351de5528d3c
i-curve <2517094646@qq.com>
2021-03-01 17:33:58 +0800

    update ubuntu.sh

--- shell/ubuntu.sh
+++ shell/ubuntu.sh
@@ -3,7 +3,8 @@
 #名称:ubuntu初始系统一键配置大全,包括更新脚本,bash配置,vim配置
 #System:Ubuntu 18.04
 #
-url_vim="https://raw.githubusercontent.com/i-curve/language/master/LINUX/vim/.vimrc"
+url_vim="https://raw.githubusercontent.com/i-curve/content/master/vimrc"
+url_hosts="https://raw.githubusercontent.com/i-curve/content/master/githubhosts"
 yellow(){
     echo -e "\033[33m\033[01m$1\033[0m"
 }
@@ -51,28 +52,8 @@ echo "export PS1=\"\\u@$st>\" " >> ~/.bashrc
 if cat /etc/hosts|grep -Eqi "github.com";then
 		echo ""
 else
-cat >> /etc/hosts <<EOF
-# GitHub Start
-52.74.223.119 github.com
-192.30.253.119 gist.github.com
-54.169.195.247 api.github.com
-185.199.111.153 assets-cdn.github.com
-151.101.76.133 raw.githubusercontent.com
-151.101.108.133 user-images.githubusercontent.com
-151.101.76.133 gist.githubusercontent.com
-151.101.76.133 cloud.githubusercontent.com
-151.101.76.133 camo.githubusercontent.com
-151.101.76.133 avatars0.githubusercontent.com
-151.101.76.133 avatars1.githubusercontent.com
-151.101.76.133 avatars2.githubusercontent.com
-151.101.76.133 avatars3.githubusercontent.com
-151.101.76.133 avatars4.githubusercontent.com
-151.101.76.133 avatars5.githubusercontent.com
-151.101.76.133 avatars6.githubusercontent.com
-151.101.76.133 avatars7.githubusercontent.com
-151.101.76.133 avatars8.githubusercontent.com
-# GitHub End
-EOF
+hosts=`curl -s $url_hosts`
+echo "$hosts" >> /etc/hosts
 fi
 #vim脚本
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
