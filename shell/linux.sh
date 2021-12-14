1b6141f465c2e70bf413fe9f5abaead426c1c517
i-curve <2517094646@qq.com>
2021-12-15 00:09:45 +0800

    version 4-beta

--- shell/linux.sh
+++ shell/linux.sh
@@ -8,32 +8,15 @@
 # 3. .vimrc 配置文件
 # 4. .tmux.conf 配置文件
 #
-url_vim="https://raw.githubusercontent.com/i-curve/config/master/vimrc"
-url_tmux="https://raw.githubusercontent.com/i-curve/config/master/tmux"
-yellow() {
-	echo -e "\033[33m\033[01m$1\033[0m"
-}
-green() {
-	echo -e "\033[32m\033[01m$1\033[0m"
-}
-red() {
-	echo -e "\033[31m\033[01m$1\033[0m"
-}
-# check 检查系统是否符合
-function check() {
-	if cat /etc/issue | grep -Eqi "debian|ubuntu|kali|deepin"; then
-		systemPackage="apt-get"
-	elif cat /etc/issue | grep -Eqi "centos|red hat|redhat"; then
-		systemPackage="yum"
-	else
-		red "系统不匹配"
-		exit 1
-	fi
-}
-check
+set -e
+source "$(dirname $0)/../util/util.sh"
 
-# install 执行安装
-function install() {
+config="https://github.com/i-curve/config.git"
+
+UtilCheck
+
+# Install 执行安装
+function Install() {
 	# 更新软件包
 	sudo $systemPackage update
 	sudo $systemPackage -y upgrade
@@ -47,91 +30,45 @@ sudo $systemPackage -y autoremove
 EOF
 	chmod +x ~/update.sh
 
-	# * 如果更新的话, 这些文件不动
-	if [[ "$1" != "update" ]]; then
-		#2. bashrc配置
-		cat >>~/.bashrc <<EOF
+	read -p "Please type the name:" -t 10 st
+	if [[ -z "$st" ]]; then st=linux; fi
+	name="export PS1=\"\\u@$st>\" "
+
+	cat >>~/.bashrc <<EOF
+ # >>> linux initialize >>>
 alias tm='tmux'
 alias rm='rm -i'
 export GOPROXY=https://goproxy.io
+$name
+4 # <<< linux initialize <<<
 EOF
-		read -p "Please type the name:" -t 10 st
-		if [[ -z "$st" ]]; then st=linux; fi
-		echo "export PS1=\"\\u@$st>\" " >>~/.bashrc
 
-		#3. vimrc配置
-		curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
-			https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
-	fi
-	curl -o ~/.vimrc $url_vim
+	3. vimrc配置
+	curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
+		https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
-	#4. tmux.conf配置
-	curl -o ~/.tmux.conf $url_tmux
-}
-# update 执行更新
-function update() {
-	green "正在卸载就组件"
-	remove update
-	green "执行安装过程"
-	install update
-}
-
-# remove 执行删除
-function remove() {
-	if [[ -e ~/update.sh ]]; then
-		rm -rf ~/update.sh
-		rm -rf ~/.vimrc
-		rm -rf ~/.tmux.conf
-		# 如果更新的话就不删不删除该文件
-		if [[ "$1" != "update" ]]; then
-			rm -rf ~/.vim
-			L=$(cat ~/.bashrc | grep -in "tmux" | cut -d: -f1)
-			k=$(cat ~/.bashrc | grep -in "export ps1" | cut -d: -f1)
-			sed -i "$L,${k}d" ~/.bashrc
-		fi
-	else
-		echo "未安装"
-	fi
+	cd ~
+	git clone $config
+	ln -s $(pwd)/config/vimrc $(pwd)/.vimrc
+	ln -s $(pwd)/config/tmux $(pwd).tmux.conf
 }
 
 #start 入口
 function start() {
-	clear
-	green "==========================="
-	green "==========================="
-	green " Author:curve"
-	green " Target:linux初始配置"
-	green " 系统：centos7+/debian9+/ubuntu16.04+"
-	green "==========================="
+	UtilEchoHead "linux初始配置"
 	echo
 	green " ====================="
-	yellow " 1. 一键安装"
-	green " ====================="
-	yellow " 2. 一键升级"
-	green " ====================="
-	yellow " 3. 一键卸载"
+	yellow " 1. 一键初始化"
 	green " ====================="
 	yellow " 0. 退出"
 	echo
 	read -p "请输入数字：" num
 	case "$num" in
 	1)
-		install
-		;;
-	2)
-		update
-		;;
-	3)
-		remove
-		;;
-	0)
-		exit 0
+		Install
 		;;
 	*)
-		clear
-		red "请输入正确数字"
-		sleep 1
-		start
+		exit 0
 		;;
 	esac
 }
---
+++ util/util.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+## bash
+# @Author: curve
+# @Date: 2021-12-14 20:16:04
+# @Last Modified by: curve
+# @Last Modified time: 2021-12-14 20:49:03
+##
+
+set -e
+
+########################const variable
+filename=$0
+filepath=$(dirname $0)
+##############################
+
+yellow() {
+    echo -e "\033[33m\033[01m$1\033[0m"
+}
+green() {
+    echo -e "\033[32m\033[01m$1\033[0m"
+}
+red() {
+    echo -e "\033[31m\033[01m$1\033[0m"
+}
+
+# check 检查系统是否符合
+function util_check() {
+    green "check system"
+    if cat /etc/issue | grep -Eqi "debian|ubuntu|kali|deepin"; then
+        systemPackage="apt-get"
+    elif cat /etc/issue | grep -Eqi "centos|red hat|redhat"; then
+        systemPackage="yum"
+    else
+        red "系统不匹配"
+        exit 1
+    fi
+}
+
+# get_version (  url flags)更新脚本
+function util_get_version() {
+    local version=$(curl -sL $1 | grep $2 | cut -d'=' -f2)
+    echo $version
+}
+
+# echo_head $1:target 输出message头部
+function util_echo_head() {
+    clear
+    green "======================================="
+    green "======================================="
+    green " Author:curve"
+    green " Target:$1"
+    green " System：centos7+/debian9+/ubuntu18.04+"
+    green "======================================="
+}
