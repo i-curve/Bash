87c5606bdeecddc69fcb7b97457e27962bde8673
i-curve <2517094646@qq.com>
2022-02-07 14:12:21 +0800

    update linux

--- shell/linux.sh
+++ shell/linux.sh
@@ -2,12 +2,7 @@
 #author: curve
 #名称:linux初始系统一键配置大全,包括更新脚本,bash配置,vim配置
 #System:Ubuntu 18.04
-#
-# 1. update.sh 更新文件
-# 2. .bashrc文件追加环境配置
-# 3. .vimrc 配置文件
-# 4. .tmux.conf 配置文件
-#
+
 set -e
 source "$(dirname $0)/../util/util.sh"
 
@@ -17,54 +12,89 @@ UtilCheck
 
 # Install 执行安装
 function Install() {
-	# 更新软件包
-	sudo $systemPackage update
-	sudo $systemPackage -y upgrade
-	sudo $systemPackage install -y git vim tmux
+    # 更新软件包
+    sudo $systemPackage update
+    sudo $systemPackage -y upgrade
+    sudo $systemPackage install -y git vim tmux
 
-	#1. update.sh配置
-	cat >~/update.sh <<EOF
-sudo $systemPackage update
-sudo $systemPackage -y dist-upgrade
-sudo $systemPackage -y autoremove
-EOF
-	chmod +x ~/update.sh
-
-	cat >>~/.bashrc <<EOF
+    cat >>~/.bashrc <<EOF
  # >>> linux initialize >>>
 alias tm='tmux'
 alias rm='rm -i'
 export GOPROXY=https://goproxy.io
 # <<< linux initialize <<<
 EOF
-
-	#3. vimrc配置
-	curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
-		https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
-
-	cd ~
-	git clone $config
-	ln -s $(pwd)/config/vimrc $(pwd)/.vimrc
-	cp $(pwd)/config/tmux ~/.tmux.conf
+    InstallShip
+    InstallVim
+    InstallUpdate
 }
 
-#start 入口
-function start() {
-	UtilEchoHead "linux初始配置"
-	echo
-	green " ====================="
-	yellow " 1. 一键初始化"
-	green " ====================="
-	yellow " 0. 退出"
-	echo
-	read -p "请输入数字：" num
-	case "$num" in
-	1)
-		Install
-		;;
-	*)
-		exit 0
-		;;
-	esac
+# InstallUpdate 安装更新脚本
+function InstallUpdate() {
+    green "安装更新脚本"
+    cat >~/update.sh <<EOF
+sudo $systemPackage update
+sudo $systemPackage -y dist-upgrade
+sudo $systemPackage -y autoremove
+EOF
+    chmod +x ~/update.sh
+    green "更新脚本OK"
 }
-start
+# InstallShip 安装ship
+function InstallShip() {
+    bash -c "$(curl -fsSL https://starship.rs/install.sh)"
+    echo 'eval "$(starship init bash)"' >>~/.bashrc
+}
+
+# InstallVim 配置vim
+function InstallVim() {
+    green "开始安装vim"
+    cd ~
+    if [[ ! -d .vim ]]; then
+        curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
+            https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim || red "vim安装失败" ||
+            exit 2
+    fi
+    if [[ ! -d config ]]; then
+        git clone $config
+    fi
+    ln -s $(pwd)/config/vimrc $(pwd)/.vimrc
+    ln -s $(pwd)/config/tmux $(pwd)/.tmux.conf
+    green "vim安装OK"
+}
+
+# Start 入口
+function Start() {
+    UtilEchoHead "linux初始配置"
+    echo
+    green " ====================="
+    yellow " 1. 一键初始化"
+    green " ====================="
+    yellow " 2. 安装starship"
+    green " ====================="
+    yellow " 3. 安装vim"
+    green " ====================="
+    yellow " 4. 安装更新脚本"
+    green " ====================="
+    yellow " 0. 退出"
+    echo
+    read -p "请输入数字：" num
+    case "$num" in
+    1)
+        Install
+        ;;
+    2)
+        InstallShip
+        ;;
+    3)
+        InstallVim
+        ;;
+    4)
+        InstallUpdate
+        ;;
+    *)
+        exit 0
+        ;;
+    esac
+}
+Start
