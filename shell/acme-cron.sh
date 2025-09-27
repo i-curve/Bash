c2a4981c7d21908d8c0c87d3d76a2b3308873f53
i-curve <i-curve@qq.com>
2025-09-27 16:17:42 +0800

    feat: add acme-cron script #13
    Signed-off-by: i-curve <i-curve@qq.com>

---
+++ shell/acme-cron.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+## bash
+# @Author: i-curve
+# @Date: 2025-09-27 16:01:55
+# @Last Modified by: i-curve
+# @Name:
+##
+
+function check_nginx() {
+    if command -v nginx >/dev/null && netstat -lnp | grep -q 80; then
+        echo 1 && exit 0
+    fi
+    echo 0 && exit 1
+}
+
+status=$(check_nginx)
+
+if [[ $status = "1" ]]; then
+    service nginx stop
+fi
+
+"/root/.acme.sh"/acme.sh --cron --home "/root/.acme.sh" >/dev/null
+
+if [[ $status = "1" ]]; then
+    service nginx start
+fi
